658eef02a12070b1ca7ebdceea12fd97fe01d09c
jrizza88 <jetorres88@gmail.com>
2016-10-15 01:09:52 -0400

    updated html files

--- Components/LoginPage.js
+++ Components/LoginPage.js
@@ -2,7 +2,17 @@ import React, {Component} from 'react';
 
 
 export default class LoginPage extends Component {
-  render() {
+  constructor(props, context) {
+
+    super(props, context);
+
+    // set initial state
+    this.state = {
+     // add modal maybe?
+    };
+  }
+    render() {
+  const { loggedIn, UserInfo, location } = this.props;
     return (
       <div id='LoginPage'>
         LoginPage
